af906dd8609a0d32620d840bfe78952a9cfa44de
Abhishek Sunil <abhisheksunil92@gmail.com>
2022-11-08 19:10:08 +0530

    add frontend components

---
+++ components/Connected.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+  Image,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+
+const Connected: FC = () => {
+  return (
+    <VStack spacing={20}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            Welcome Solana Wiz.
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each solana wiz is randomly generated and can be staked to receive
+            <Text as="b"> $WIZ</Text> Use your <Text as="b"> $WIZ</Text> to
+            upgrade your solana wiz and receive perks within the community!
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={10}>
+        <Image src="images/avatar1.png" alt="" />
+        <Image src="images/avatar2.png" alt="" />
+        <Image src="images/avatar3.png" alt="" />
+        <Image src="images/avatar4.png" alt="" />
+        <Image src="images/avatar5.png" alt="" />
+      </HStack>
+
+      <Button bgColor="accent" color="white" maxW="380px">
+        <HStack>
+          <Text>mint solana wiz</Text>
+          <ArrowForwardIcon />
+        </HStack>
+      </Button>
+    </VStack>
+  );
+};
+
+export default Connected;
