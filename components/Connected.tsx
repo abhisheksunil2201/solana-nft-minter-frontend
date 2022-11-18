13cc4cc11876613a3737ed755ceee4eb814ed098
Abhishek Sunil <abhisheksunil92@gmail.com>
2022-11-18 23:06:01 +0530

    add mint feature

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -1,16 +1,80 @@
-import { FC } from "react";
 import {
   Button,
   Container,
   Heading,
-  HStack,
-  Text,
   VStack,
+  Text,
+  HStack,
   Image,
 } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import {
+  FC,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { PublicKey } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  Metaplex,
+  walletAdapterIdentity,
+  CandyMachineV2,
+} from "@metaplex-foundation/js";
+import { useRouter } from "next/router";
 
 const Connected: FC = () => {
+  const { connection } = useConnection();
+  const walletAdapter = useWallet();
+  const [candyMachine, setCandyMachine] = useState<CandyMachineV2>();
+  const [isMinting, setIsMinting] = useState(false);
+
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter));
+  }, [connection, walletAdapter]);
+
+  useEffect(() => {
+    if (!metaplex) return;
+
+    metaplex
+      .candyMachinesV2()
+      .findByAddress({
+        address: new PublicKey("ASDQkVhEDJeiraBUfgbDo2xPkr1ja5HJQtx5rh7eLhD5"),
+      })
+      .then((candyMachine) => {
+        console.log(candyMachine);
+        setCandyMachine(candyMachine);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [metaplex]);
+
+  const router = useRouter();
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {
+      if (event.defaultPrevented) return;
+
+      if (!walletAdapter.connected || !candyMachine) {
+        return;
+      }
+
+      try {
+        setIsMinting(true);
+        const nft = await metaplex.candyMachinesV2().mint({ candyMachine });
+        console.log(nft);
+        router.push(`/newMint?mint=${nft.nft.address.toBase58()}`);
+      } catch (error) {
+        alert(error);
+      } finally {
+        setIsMinting(false);
+      }
+    },
+    [metaplex, walletAdapter, candyMachine]
+  );
+
   return (
     <VStack spacing={20}>
       <Container>
@@ -22,30 +86,33 @@ const Connected: FC = () => {
             noOfLines={1}
             textAlign="center"
           >
-            Welcome Solana Wiz.
+            Welcome Wizard.
           </Heading>
 
           <Text color="bodyText" fontSize="xl" textAlign="center">
-            Each solana wiz is randomly generated and can be staked to receive
-            <Text as="b"> $WIZ</Text> Use your <Text as="b"> $WIZ</Text> to
-            upgrade your solana wiz and receive perks within the community!
+            Each SOL Wizard is randomly generated and can be staked to receive
+            <Text as="b"> $WIZ</Text>. Use your <Text as="b"> $WIZ</Text> to
+            upgrade your wizard and receive perks within the community!
           </Text>
         </VStack>
       </Container>
 
       <HStack spacing={10}>
-        <Image src="images/avatar1.png" alt="" />
-        <Image src="images/avatar2.png" alt="" />
-        <Image src="images/avatar3.png" alt="" />
-        <Image src="images/avatar4.png" alt="" />
-        <Image src="images/avatar5.png" alt="" />
+        <Image width={"100px"} height={"100px"} src="images/0.png" alt="" />
+        <Image width={"100px"} height={"100px"} src="images/1.png" alt="" />
+        <Image width={"100px"} height={"100px"} src="images/2.png" alt="" />
+        <Image width={"100px"} height={"100px"} src="images/3.png" alt="" />
+        <Image width={"100px"} height={"100px"} src="images/4.png" alt="" />
       </HStack>
 
-      <Button bgColor="accent" color="white" maxW="380px">
-        <HStack>
-          <Text>mint solana wiz</Text>
-          <ArrowForwardIcon />
-        </HStack>
+      <Button
+        bgColor="accent"
+        color="white"
+        maxW="380px"
+        onClick={handleClick}
+        isLoading={isMinting}
+      >
+        <Text>Mint sol wiz</Text>
       </Button>
     </VStack>
   );
---
+++ pages/newMint.tsx
@@ -0,0 +1,106 @@
+import type { NextPage } from "next";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import MainLayout from "../components/MainLayout";
+import {
+  Container,
+  Heading,
+  VStack,
+  Text,
+  Image,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
+import {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { PublicKey } from "@solana/web3.js";
+import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+
+const NewMint: NextPage<NewMintProps> = ({ mint }) => {
+  const [metadata, setMetadata] = useState<any>();
+  const { connection } = useConnection();
+  const walletAdapter = useWallet();
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter));
+  }, [connection, walletAdapter]);
+
+  useEffect(() => {
+    metaplex
+      .nfts()
+      .findByMint({ mintAddress: new PublicKey(mint) })
+      .then((nft) => {
+        fetch(nft.uri)
+          .then((res) => res.json())
+          .then((metadata) => {
+            setMetadata(metadata);
+          });
+      });
+  }, [mint, metaplex, walletAdapter]);
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {},
+    []
+  );
+
+  return (
+    <MainLayout>
+      <VStack spacing={20}>
+        <Container>
+          <VStack spacing={8}>
+            <Heading color="white" as="h1" size="2xl" textAlign="center">
+              A new wizard has appeared!
+            </Heading>
+
+            <Text color="bodyText" fontSize="xl" textAlign="center">
+              Congratulations, you minted a lvl 1 Wizard! <br />
+              Time to stake your character to earn rewards and level up.
+            </Text>
+          </VStack>
+        </Container>
+
+        <Image
+          width={"100px"}
+          height={"100px"}
+          src={metadata?.image ?? ""}
+          alt=""
+        />
+
+        <Button
+          bgColor="accent"
+          color="white"
+          maxW="380px"
+          onClick={handleClick}
+        >
+          <HStack>
+            <Text>Stake my wizard</Text>
+            <ArrowForwardIcon />
+          </HStack>
+        </Button>
+      </VStack>
+    </MainLayout>
+  );
+};
+
+interface NewMintProps {
+  mint: PublicKey;
+}
+
+NewMint.getInitialProps = async ({ query }) => {
+  const { mint } = query;
+
+  if (!mint) throw { error: "no mint" };
+
+  try {
+    const mintPubkey = new PublicKey(mint);
+    return { mint: mintPubkey };
+  } catch {
+    throw { error: "invalid mint" };
+  }
+};
+
+export default NewMint;
